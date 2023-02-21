4babe56801d9b7529cc67f9b6dab61b150844918
Davide Giacomini <giacomini.davide@outlook.com>
2023-02-21 10:59:24 -0600

    Check tot rows updated

--- check_tot_rows.py
+++ check_tot_rows.py
@@ -13,16 +13,33 @@ def count_cluster_csv_rows(startpath, K):
             # if k subdirectory doesn't exist, count rows in k_cluster.csv files
             for file in os.listdir(startpath):
                 if file == f'{k}_cluster.csv':
-                    df = pd.read_csv(os.path.join(startpath, file))
-                    total_rows += len(df)+1
+                    df = pd.read_csv(os.path.join(startpath, file), header=None)
+                    total_rows += len(df)
+
+                    # Append the DataFrame to a file to check
+                    df.to_csv('check_50.csv', index=False, mode='a', header=False)
+
             print(f"{k} has {total_rows} rows in cluster CSV files.")
 
     return total_rows
 
 tot_rows = count_cluster_csv_rows('training_table_50_clusters', 32)
 
-df = pd.read_csv('training_table_50.csv', header=None)
+df_origin = pd.read_csv('training_table_50.csv', header=None)
+df_check = pd.read_csv('check_50.csv', header=None)
 
 print(tot_rows)
-print(len(df))
-print(len(df) == tot_rows)
+print(len(df_origin))
+print(len(df_check))
+print(len(df_origin) == tot_rows)
+
+# Sort the rows of the first DataFrame based on every column from left to right
+df_origin = df_origin.sort_values(by=df_origin.columns.tolist()).reset_index(drop=True)
+
+# Sort the rows of the second DataFrame based on every column from left to right
+df_check = df_check.sort_values(by=df_check.columns.tolist()).reset_index(drop=True)
+
+if df_origin.equals(df_check):
+    print('EQUAL')
+else:
+    print('NOT EQUAL')
